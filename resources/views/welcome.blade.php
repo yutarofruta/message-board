c72db03dd099688aa17c24a8616bbb6ec7b2776e
yutaro.furuta <furutaro123@gmail.com>
2019-05-26 11:54:15 +0000

    set timezone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    太郎
                 </div>
 
                 <div class="links">
